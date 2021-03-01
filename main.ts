0d524bfd10129161096c395fb03f3513a374fe76
bayramkarahan <44923603+bayramkarahan@users.noreply.github.com>
2021-03-01 19:44:04 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+basic.showNumber(input.lightLevel())
+basic.forever(function () {
+    if (input.lightLevel() > 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (input.lightLevel() < 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
